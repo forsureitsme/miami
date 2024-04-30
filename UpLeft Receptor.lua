718bf9198f4bcdc528179afb35e562f2f16a91d8
Pedro Cardoso da Silva (@forsureitsme) <forsureitsme@gmail.com>
2024-04-30 20:05:48 -0300

    First attempt at receptors (outline press is ugly)

--- UpLeft Receptor.lua
+++ UpLeft Receptor.lua
@@ -1,7 +1,6 @@
 local player = Var "Player" or GAMESTATE:GetMasterPlayerNumber()
 
 local function Beat(self)
-	-- too many locals
 	local this = self:GetChildren()
 	local playerstate = GAMESTATE:GetPlayerState( player )
 	local songposition = playerstate:GetSongPosition() -- GAMESTATE:GetSongPosition()
@@ -22,43 +21,25 @@ return Def.ActorFrame {
 	-- COMMANDS --
 	InitCommand=cmd(SetUpdateFunction,Beat);
 	
-	-- LAYERS --
-	NOTESKIN:LoadActor("Center", "Outline Receptor")..{
-		Name="Outline Full";
-		Condition=Var "Button" == "Center" and GAMESTATE:GetCurrentStyle():GetStepsType() ~= 'StepsType_Pump_Halfdouble';
-		--InitCommand=cmd(x,96);
-	};
-	NOTESKIN:LoadActor("DownLeft", "Outline Receptor")..{
-		Name="Outline Half";
-		Condition=Var "Button" == "DownLeft" and GAMESTATE:GetCurrentStyle():GetStepsType() == 'StepsType_Pump_Halfdouble';
-		--InitCommand=cmd(x,96);
-	};
-	
 	NOTESKIN:LoadActor(Var "Button", "Ready Receptor")..{
 		Name="Base";
 		Frames={
 			{ Frame = 0 }
 		};
-		PressCommand=cmd(finishtweening;linear,0.05;zoom,0.9;linear,0.1;zoom,1);
 	};
 	NOTESKIN:LoadActor(Var "Button", "Ready Receptor")..{
 		Name="Glow";
 		Frames= {
 			{ Frame = 1 }
 		};
-		InitCommand=cmd(blend,'BlendMode_Add');
-		PressCommand=cmd(finishtweening;linear,0.05;zoom,0.9;linear,0.1;zoom,1);
 	};
-	--[[
 	NOTESKIN:LoadActor(Var "Button", "Ready Receptor")..{
-		Name="Tap";
-		Frames = { Frame = 2 };
-		InitCommand=cmd(zoom,1;diffusealpha,0;glow,1,1,1,0);
-		--NOTESKIN:GetMetricA(Var "Button", "TapInitCommand");
-		--
-		PressCommand=cmd(finishtweening;glow,1,1,1,1;zoom,1;linear,0.2;glow,1,1,1,0;zoom,1.2);
-		--NOTESKIN:GetMetricA(Var "Button", "TapHeldCommand");
-		--
+		Name="Outline";
+		Frames= {
+			{ Frame = 2 }
+		};
+		InitCommand=cmd(zoom,1;diffusealpha,0;blend,'BlendMode_Add');
+		PressCommand=cmd(stoptweening;zoom,1;linear,0.1;diffusealpha,1);
+		LiftCommand=cmd(stoptweening;diffusealpha,1;zoom,1;linear,0.1;zoom,1.1;diffusealpha,0);
 	};
-	--]]
 }
