4801e549d2d32d6b563b56babcd6f57fb5d97851
Pedro Cardoso da Silva (@forsureitsme) <forsureitsme@gmail.com>
2024-05-06 18:43:34 -0300

    WIP Tap Note Explosion and Receptor Press

--- UpLeft Explosion.lua
+++ UpLeft Explosion.lua
@@ -1,6 +1,8 @@
 return Def.ActorFrame {
-	--note graphic
-	NOTESKIN:LoadActor(Var "Button", "Tap Note") .. {
+	NOTESKIN:LoadActor(Var "Button", "Ready Receptor") .. {
+		Frames= {
+			{ Frame = 0 }
+		};
 		InitCommand=cmd(blend,"BlendMode_Add";playcommand,"Glow");
 		W1Command=cmd(playcommand,"Glow");
 		W2Command=cmd(playcommand,"Glow");
@@ -9,20 +11,10 @@ return Def.ActorFrame {
 		W5Command=cmd();
 		
 		HitMineCommand=cmd(playcommand,"Glow");
-		GlowCommand=cmd(setstate,0;finishtweening;diffusealpha,1.0;zoom,1.0;linear,0.15;diffusealpha,0.9;zoom,1.15;linear,0.15;diffusealpha,0.0;zoom,1.3);
+		GlowCommand=cmd(finishtweening;zoom,1;diffusealpha,1;accelerate,0.4;diffusealpha,0;zoom,1.2);
 		HeldCommand=cmd(playcommand,"Glow");
-	};
-	--tap
-	NOTESKIN:LoadActor(Var "Button", "Ready Receptor")..{
-		Name="Tap";
-		--Frames = { { Frame = 2 ; Delay = 1 } };
-		TapCommand=cmd(finishtweening;diffusealpha,1;zoom,1;linear,0.2;diffusealpha,0;zoom,1.2);
-		InitCommand=cmd(pause;setstate,2;playcommand,"Tap");
-		HeldCommand=cmd(playcommand,"Tap");
-		ColumnJudgmentMessageCommand=cmd(playcommand,"Tap");
-		--TapNoneCommand=cmd(playcommand,"Tap");
-	};
-	--explosion
+	};	
+
 	LoadActor("_flash")..{
 		InitCommand=cmd(blend,"BlendMode_Add";playcommand,"Glow");
 		W1Command=cmd(playcommand,"Glow");
@@ -30,14 +22,18 @@ return Def.ActorFrame {
 		W3Command=cmd(playcommand,"Glow");
 		W4Command=cmd();
 		W5Command=cmd();
-		--HoldingOnCommand=cmd(playcommand,"Glow");
+
 		HitMineCommand=cmd(playcommand,"Glow");
 		HeldCommand=cmd(playcommand,"Glow");
-		GlowCommand=cmd(setstate,0;finishtweening;diffusealpha,1;zoom,1;linear,0.2;diffusealpha,0;zoom,1.2);
-	};
-	--thing...
-	Def.Quad {
-		InitCommand=cmd(zoomto,50,5000;diffusealpha,0);
-		HitMineCommand=cmd(finishtweening;diffusealpha,1;linear,0.3;diffusealpha,0);
+		-- GlowCommand=cmd(finishtweening;setstate,0;diffusealpha,1;zoom,1;accelerate,0.2;diffusealpha,0;zoom,1.2);
+		GlowCommand=cmd(
+			finishtweening;
+			rotationz,math.random(0,360);
+			zoom,.9;
+			diffusealpha,1;
+			decelerate,.3;
+			diffusealpha,0;
+			zoom,.75;
+		);
 	};
 }
